63d48d041a568954817132bf4c3e8a03371fee0c
tirth2864 <112847085+tirth2864@users.noreply.github.com>
2022-12-02 00:04:54 +0530

    Loader

---
+++ src/pages/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className="min-h-min min-w-min flex justify-center align-middle">
+            <div className="border-t-transparent border-solid animate-spin rounded-full border-spicy border-8 h-32 w-32"></div>
+    </div>
+  )
+}
+
+export default Loader;
